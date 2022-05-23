ce3dabd83a8f7bbf964cea19d2cca592f7a4f98f
Rawan_Elbana <ciaorona633@gmail.com>
2022-05-23 22:53:29 +0200

    Time: 1175 ms (33.63%), Space: 18 MB (52.72%) - LeetHub

--- 860-lemonade-change/860-lemonade-change.py
+++ 860-lemonade-change/860-lemonade-change.py
@@ -2,23 +2,17 @@ class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
         five = 0
         ten = 0
-        twen = 0
         for num in bills:
             if num != 5:
                 if num == 10:
                     if five > 0:
-                        five -= 1
-                        ten += 1
+                        five -= 1; ten += 1
                     else: return False
-
-                if num == 20:
-                    if ten > 0 and five > 0:
-                        five -= 1
-                        ten -= 1
-                        twen += 1
-                    elif five >= 3:
-                        five -=3
-                    else: return False
+                elif ten > 0 and five > 0:
+                    five -= 1;ten -= 1
+                elif five >= 3:
+                    five -=3
+                else: return False
             else:
                 five += 1
 
