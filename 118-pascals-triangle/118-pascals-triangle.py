2b1c6b56ce511f59ace9ce739292aa93d0cd67d9
Rawan_Elbana <ciaorona633@gmail.com>
2022-07-19 10:43:08 +0200

    Time: 51 ms (40.48%), Space: 14 MB (17.88%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -0,0 +1,13 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        result = [[1]]
+        countOfRows = range(numRows - 1)
+        
+        if numRows >= 1 or numRows <= 30:
+            for i in countOfRows:
+                temp = [0] + result[-1] + [0]
+                row = []
+                for x in range(len(result[-1]) + 1):
+                    row.append(temp[x] + temp[x+1])
+                result.append(row)
+        return result
