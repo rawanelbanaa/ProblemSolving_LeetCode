b73bfdb37cd1a08bb26d24ddcc216f244735d2b8
Rawan_Elbana <ciaorona633@gmail.com>
2022-08-05 00:40:47 +0200

    Time: 63 ms (15.84%), Space: 13.8 MB (63.37%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.py
+++ 858-mirror-reflection/858-mirror-reflection.py
@@ -1,13 +1,11 @@
 class Solution:
     def mirrorReflection(self, p: int, q: int) -> int:
-        ext = q 
-        ref = p
         
-        while ext % 2==0 and ref % 2 == 0:
-            ext /= 2
-            ref /= 2
+        while q % 2==0 and p % 2 == 0:
+            q /= 2
+            p /= 2
             
-        if ext % 2 ==0 and ref % 2 !=0: return 0
-        if ext % 2 ==1 and ref % 2 ==0: return 2
-        if ext % 2 ==1 and ref % 2 !=0: return 1
+        if q % 2 ==0 and p % 2 !=0: return 0
+        if q % 2 ==1 and p % 2 ==0: return 2
+        if q % 2 ==1 and p % 2 !=0: return 1
         
