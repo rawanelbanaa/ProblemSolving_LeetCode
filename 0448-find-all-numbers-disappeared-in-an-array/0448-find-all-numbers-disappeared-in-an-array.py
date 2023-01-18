f9f9fa5260b479ad636d2caa27996bb480866423
Rawan_Elbanna <ciaorona633@gmail.com>
2023-01-18 10:40:15 +0200

    0448-find-all-numbers-disappeared-in-an-array.py

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -4,18 +4,3 @@ class Solution:
             return []
         n = len(nums)
         return list(set(range(1,n+1)) - set(nums))
-    
-#     solution1
-#         Res= []
-#         n = len(nums)
-#         for i in range(1,n+1):
-#             if i in nums:
-#                 continue
-#             else:
-#                 Res.append(i)
-        
-#         return Res
-
-       
-# solution2
-# return set(range(1, len(nums)+1)).difference(set(nums))
